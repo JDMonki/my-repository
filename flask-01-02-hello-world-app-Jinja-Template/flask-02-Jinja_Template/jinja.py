7462ef5137cda1d2535cbb354a1dbc5b9ea21e7a
Andrew McMillin <mcmillinap@gmail.com>
2022-12-05 20:00:53 -0700

    first flask files

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def head():
+    return render_template('index.html', number1=10, number2=20)
+@app.route('/number/<string:num1>')
+def number(num1):
+    return render_template('index.html', number1=num1, number2=20)
+if __name__ == '__main__':
+    app.run(debug=True)
